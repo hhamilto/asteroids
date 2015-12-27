cea61c6f9e4b350f3762ae10031ce25b29e91a0f
Hurricane Hamilton <hhamilto@mtu.edu>
2015-12-27 16:43:53 -0500

    Fix memory gush

--- public/js/sound-manager.js
+++ public/js/sound-manager.js
@@ -1,9 +1,16 @@
 SoundManager = (function(){
+	var SOUND_ARRAY_SIZE = 30
 	var pew = new Audio('sound/pew.mp3')
+	var pews = []
+	var i = SOUND_ARRAY_SIZE
+	do
+		pews.push(pew.cloneNode())
+	while(--i)
 	var BindShip = function(ship){
 		ship.on('bullet', function(){
-			pew.cloneNode().play()
-			//pew.play()
+			pews[i++].play()
+			i=i%SOUND_ARRAY_SIZE//pew.play()
+			console.log(i, pews.length)
 		})
 	}
 	return {
